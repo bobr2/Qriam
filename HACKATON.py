4c7e58c681ba36634678e900f417c641cb4459bf
Nex1sG <128090001+Nex1sG@users.noreply.github.com>
2024-11-15 22:11:53 +0300

    Update HACKATON.py

--- HACKATON.py
+++ HACKATON.py
@@ -5,12 +5,13 @@ url_1 = "Sprints.csv" # Ему соответствует arr_s
 url_2 = "History.csv" # Ему соответствует arr_h
 url_3 = "Data.csv" # Ему соответствует arr_d
 
-arr_s.columns = ("sprint_name", "sprint_status", "sprint_start", "sprint_end", "entity_ids")
-
 
 with open(url_1, encoding="utf-8") as f: arr_s = pd.DataFrame([i for i in csv.reader(f, delimiter=";")][2:])
 with open(url_2, encoding="utf-8") as f: arr_h = pd.DataFrame([i for i in csv.reader(f, delimiter=";")][2:])
 with open(url_3, encoding="utf-8") as f: arr_d = pd.DataFrame([i for i in csv.reader(f, delimiter=";")][2:])
+
+
+arr_s.columns = ("sprint_name", "sprint_status", "sprint_start", "sprint_end", "entity_ids")
 
 print(arr_s)
 print(arr_h)
