5512277712f486d9ffd09e0861c2cadd85e1ca3c
Nex1sG <128090001+Nex1sG@users.noreply.github.com>
2024-11-15 22:27:13 +0300

    Update HACKATON.py

--- HACKATON.py
+++ HACKATON.py
@@ -1,4 +1,3 @@
-
 import csv
 import pandas as pd
 
@@ -6,17 +5,13 @@ url_1 = "Sprints.csv" # Ему соответствует arr_s
 url_2 = "History.csv" # Ему соответствует arr_h
 url_3 = "Data.csv" # Ему соответствует arr_d
 
-
 with open(url_1, encoding="utf-8") as f: arr_s = pd.DataFrame([i for i in csv.reader(f, delimiter=";")][2:])
-with open(url_2, encoding="utf-8") as f: arr_h = pd.DataFrame([i for i in csv.reader(f, delimiter=";")][2:])
+with open(url_2) as f: arr_h = pd.DataFrame([i for i in csv.reader(f, delimiter=";")][2:])
 with open(url_3, encoding="utf-8") as f: arr_d = pd.DataFrame([i for i in csv.reader(f, delimiter=";")][2:])
-
 
 arr_s.columns = ("sprint_name", "sprint_status", "sprint_start", "sprint_end", "entity_ids")
 arr_h.columns = ("entity_id", "history_property_name", "history_date", "history_version", "history_change_type", "history_change")
 arr_d.columns = ("entity_id", "area", "type",	"status", "state", "priority","ticket_number", "name", "create_date", "created_by",	"update_date",	"updated_by",	"parent_ticket_id",	"assignee",	"owner", "due_date", "rank",	"estimation",	"spent",	"workgroup",	 "resolution")
-
-
 
 print(arr_s)
 print(arr_h)
