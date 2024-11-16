a4f976aaffb6fa6ce324acb73f665af2568214bf
Igor <g0kanik@yandex.ru>
2024-11-16 11:33:44 +0300

    12

--- entities_of_sprint.py
+++ entities_of_sprint.py
@@ -1,5 +1,5 @@
-def ent(sprint,table):
-    entity=table['entity_ids'].loc[sprint-1].replace('{','',1)
+def ent(sprint,arr_s):
+    entity=arr_s['entity_ids'].loc[sprint-1].replace('{','',1)
     entity=entity.replace('}','',1)
     entity=entity.split(',')
     entity=list(map(int,entity))
