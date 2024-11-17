808daad56db15697a71018d8f8eb4409a8b876c9
maks <matius_d@list.ru>
2024-11-17 11:36:55 +0300

    Add files via upload

--- find_Id.py
+++ find_Id.py
@@ -1,16 +1,19 @@
 
 
+def ent(sprint,area,arr_s, arr_d): #Получаем id
+    indx = arr_s["sprint_name"] == sprint
+    entity= ((arr_s.loc[indx])['entity_ids'].values)[0]
 
-
-def ent(sprint,table): #Получаем id
-    indx = table["sprint_name"] == sprint
-    entity= ((table.loc[indx])['entity_ids'].values)[0]
+    entit_ran = arr_d[arr_d["area"] == area]["entity_id"]
 
 
     entity = entity.replace('{','',1)
     entity=entity.replace('}','',1)
     entity=entity.split(',')
     entity=list(map(int,entity))
+
+    inter = set(entit_ran) & set(entity)
+    entity = list(inter)
 
     return entity
 
